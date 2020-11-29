4efcf5a475b0e2aa4c824cf34fd087f62d73a5a6
jindalakhil <akhiljindal222@gmail.com>
2020-11-29 22:10:53 +0530

    [Akhil]Added: React Basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import App2 from "./App2"
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends React.Component{
+//   render (){
+//     // return (
+//     //   <div>
+//     //     <h1>Hello React-06</h1>
+//     //   </div>
+//     // );
+//     return <div><App2></App2>
+//     <App2></App2>
+//     <App2></App2>
+//     <App2></App2></div>
+//   }
+// }
+
+class App extends React.Component {
+  url= 'https://www.google.com'
+  constructor() {
+    super()
+    this.state = {
+      userName: '',
+      title: "hello from constructor defined state"
+    }
+  }
+  //onClick function
+  onClick = ($event) =>{
+    console.log("save button is clicked", $event);
+    window.open(this.url,"_blank")
+  }
+  onNameChange=(event)=>{
+    console.log("Value is",event.target.value)
+    this.setState({userName: event.target.value})
+  }
+    
+  render(){
+    return (
+      <div>
+        <h1>Hello {this.state.userName} user dynamically changes</h1>
+        <img src={logo} onClick={this.onClick}
+        alt = "React logo" width="50px" />
+        <div className="text-box">
+          <input onChange={this.onNameChange}/>
+       </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/App2.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react"
+
+class App extends React.Component{
+  render (){
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
